1e899c55c1105aa24325a504ada6e2bbd8f1b874
Johnranletw <110260255+Johnranletw@users.noreply.github.com>
2023-01-11 21:47:33 -0500

    class activites

--- 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -1,3 +1,6 @@
 // Create a variable below
 
-// Console.log() your variable here.
+var helloWorld = 'Hello World';
+
+console.log(helloWorld)
+
--- 19.1-activities/05_Build_a_toggle/unsolved/js/index.js
+++ 19.1-activities/05_Build_a_toggle/unsolved/js/index.js
@@ -1,1 +1,9 @@
 // Add Event Listener Below
+
+$("#toggle").on("click", function slideToggle(){
+$("ul").slideToggle()
+});
+
+$("li").on("click", function hiddenthing(){
+    $("li").css("display","none")
+})
